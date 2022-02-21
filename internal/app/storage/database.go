94c585e7337dd54b90e4bca019ec4c89207d4da3
Maximiliank <48432756+EestiChameleon@users.noreply.github.com>
2022-02-21 12:16:16 +0500

    инкремент 5 добавлен парсинг SERVER_ADDRESS & BASE_URL

--- internal/app/storage/database.go
+++ internal/app/storage/database.go
@@ -59,7 +59,7 @@ func ShortURL() (shortedURL string, err error) {
 	if err != nil {
 		return "", err
 	}
-	if cfg.Envs.BaseURL == "" {
+	if cfg.Envs.BaseURL != "" {
 		shortedURL = fmt.Sprintf("%s/%x", cfg.Envs.BaseURL, b[0:])
 	} else {
 		shortedURL = fmt.Sprintf("%s/%x", ShortLinkHost, b[0:])
