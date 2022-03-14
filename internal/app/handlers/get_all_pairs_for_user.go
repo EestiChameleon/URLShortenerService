f1df03c1ee604e574afa05800cd90df34e8156fa
Maximiliank <48432756+EestiChameleon@users.noreply.github.com>
2022-03-14 18:49:57 +0500

    increment 11 4 check GetAllPairs - pairs check condition improved

--- internal/app/handlers/get_all_pairs_for_user.go
+++ internal/app/handlers/get_all_pairs_for_user.go
@@ -10,11 +10,13 @@ import (
 func GetAllPairs(w http.ResponseWriter, r *http.Request) {
 	log.Println("GetAllPairs start: search pairs")
 	pairs, err := storage.User.GetUserURLs()
-	if err != nil {
-		log.Println("GetAllPairs: user pairs not found -> 204")
+	log.Println(pairs)
+	if err != nil || len(pairs) == 0 {
+		log.Println("GetAllPairs: user pairs not found - ", err)
 		resp.NoContent(w, http.StatusNoContent)
 		return
 	}
+
 	log.Println("GetAllPairs end: user pairs found -> JSON 200")
 	resp.JSON(w, http.StatusOK, pairs)
 }
